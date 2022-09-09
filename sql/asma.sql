0fcaea0ba7dda82eb229a263a42b7a5b17741897
SosaEm <emsosa50@gmail.com>
2022-09-09 15:19:42 -0300

    9/9

--- sql/asma.sql
+++ sql/asma.sql
@@ -21,3 +21,20 @@ WHERE fecha = (
     FROM controles AS b
     WHERE a.codigo_id = b.codigo_id
 )
+
+
+select codigo_id,max(fecha) 
+from (select max(fecha) as fecha from epi
+union
+select max(fecha) as fecha from inter);
+
+select u.codigo_id
+     , greatest(u.fecha,max(s.fecha),max(d.fecha),max(e.fecha))
+  from epi u
+     , inter s
+     , clinica d
+     , controles e             
+ where s.codigo_id = u.codigo_id
+ group by u.codigo_id
+     , u.codigo_id
+     , u.fecha
