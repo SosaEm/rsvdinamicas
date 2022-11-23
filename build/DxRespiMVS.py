e67ec4802eb1c8b7d79e408f433b506181d49cd5
SosaEm <emsosa50@gmail.com>
2022-11-23 14:11:21 -0300

    23/11

--- build/DxRespiMVS.py
+++ build/DxRespiMVS.py
@@ -9,7 +9,7 @@ def DxRespiMVS():
                     passwd=credenciales.get("DATABASE_PASSWORD"),
                     db="rsv dinamicas") as conn:
         cur = conn.cursor()
-        sql = "SELECT record_id,hospital,dx_respi___1,dx_respi___2,dx_respi___3,dx_respi___4,dx_respi___5,dx_respi___6,dx_respi___7,dx_respi___0 FROM rsvdinamicas WHERE dx_respi___1=0 and dx_respi___2=0 and dx_respi___3=0 and dx_respi___4=0 and dx_respi___5=0 and dx_respi___6=0 and dx_respi___7=0 and dx_respi___0=0;"
+        sql = "SELECT record_id,hospital,dx_respi___1,dx_respi___2,dx_respi___3,dx_respi___4,dx_respi___5,dx_respi___6,dx_respi___7,dx_respi___0 FROM rsvdinamicas WHERE dx_respi___1=0 and dx_respi___2=0 and dx_respi___3=0 and dx_respi___4=0 and dx_respi___5=0 and dx_respi___6=0 and dx_respi___7=0 and dx_respi___0=0 and fecha_alta_irab is not NULL;"
         cur.execute(sql)
         data = cur.fetchall()
         cur.close() #Ver si hace falta cerrar la conexion
